590371dc4dc1514a9217a09e93facdb5679c8f2a
A111223013 <a111223013@mail.shu.edu.tw>
2024-04-26 14:26:24 +0800

    2024-04-26

--- Ch5_5/app/src/main/java/com/example/ch5_5/MainActivity.java
+++ Ch5_5/app/src/main/java/com/example/ch5_5/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -29,11 +32,13 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     @Override
     public boolean onTouch(View v, MotionEvent event){
         int act = event.getAction();
+        Vibrator vb = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
         switch (act)
         {
             case MotionEvent.ACTION_DOWN:
                 txvOutput.setText("Action_DOWN");
                 txvOutput.setTextColor(Color.RED);
+                //vb.vibrate(VibrationEffect.createOneShot(2000, 20));
                 break;
             case MotionEvent.ACTION_UP:
                 txvOutput.setText("Action_UP");
